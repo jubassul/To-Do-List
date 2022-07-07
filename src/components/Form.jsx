fccc673215344706a5bfd73f5b8a5c968d0d1eb3
Júlia Bassul <jubassul@gmail.com>
2022-07-07 11:25:32 -0300

    change state from input and add function onclick from the button

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
-import Button from '@mui/material/Button';
-
+import Button from "@mui/material/Button";
+import { useState } from "react";
 export default function Form() {
-    return (
-        <> 
-        
-        <div className="form">
-           <h1>To Do List:</h1>
-            <TextField id="outlined-basic" label="Outlined" 
-            variant="outlined" fullWidth  />
-            <Button  className='button'> Add </Button> 
-        </div>
-
-        </>
-    );
+  const [input, setInput] = useState("teste");
+  const addToList = () => {
+    alert("O que o usuário digitou " + input);
+  };
+  return (
+    <>
+      <div className="form">
+        <h1>To Do List:</h1>
+        <TextField
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+          fullWidth
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <Button className="button" onClick={addToList}>
+          Add
+        </Button>
+      </div>
+    </>
+  );
 }
